524bf8a9684b0c1c90ec5bfec6d60af596cbc0fc
Rashid Khan <rashood.khan@gmail.com>
2014-07-09 03:24:54 +0400

    Added Test case for Factorial implementation using recursion

--- SimpleVM/Test.cs
+++ SimpleVM/Test.cs
@@ -5,7 +5,7 @@ namespace SimpleVM
 	public class Test
 	{
 		public static void Main(){
-			VM vm= new VM (hello, 0, 1);
+			VM vm= new VM (factorial, 22, 0);
 			vm.trace = true;
 			vm.cpu ();
 		}
@@ -16,6 +16,32 @@ namespace SimpleVM
 			ByteCode.IMUL,
 			ByteCode.PRINT,
 			ByteCode.HALT
+		};
+
+		static int[] factorial = {
+			//Define Factorial : Less than 2 -> return 1
+										//Address
+			ByteCode.LOAD, -3,			//0
+			ByteCode.ICONST, 2,			//2
+			ByteCode.ILT,				//4
+			ByteCode.BRF, 10,			//5
+			ByteCode.ICONST,1,			//7
+			ByteCode.RET,				//9
+
+			//Returs N * FACT(N-1)
+			ByteCode.LOAD,-3,			//10
+			ByteCode.LOAD,-3,			//12
+			ByteCode.ICONST, 1,			//14
+			ByteCode.ISUB,				//16
+			ByteCode.CALL, 0, 1,		//17
+			ByteCode.IMUL,				//20
+			ByteCode.RET,				//21
+
+			//Main Method PRINT FACT(5)
+			ByteCode.ICONST,4,			//22
+			ByteCode.CALL,0,1,			//24
+			ByteCode.PRINT,				//27
+			ByteCode.HALT				//28
 		};
 
 		public Test () {

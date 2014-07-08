41d386d8d07f73b493785b539261a8b21707bae6
Rashid Khan <rashood.khan@gmail.com>
2014-07-08 18:07:43 +0400

    Added implementation for Global Load and Store

--- SimpleVM/Test.cs
+++ SimpleVM/Test.cs
@@ -5,13 +5,15 @@ namespace SimpleVM
 	public class Test
 	{
 		public static void Main(){
-			VM vm= new VM (hello, 0, 0);
+			VM vm= new VM (hello, 0, 1);
 			vm.trace = true;
 			vm.cpu ();
 		}
 
 		static int[] hello = {
 			ByteCode.ICONST, 20202,
+			ByteCode.GSTORE, 0,
+			ByteCode.GLOAD, 0,
 			ByteCode.PRINT,
 			ByteCode.HALT
 		};
--- SimpleVM/VM.cs
+++ SimpleVM/VM.cs
@@ -9,6 +9,7 @@ namespace SimpleVM
 		int[] data;			//Data Memory
 		int[] code;			//Code Memory
 		int[] stack;		//Stack
+		int[] globals;
 
 		int ip;				//Instruction Pointer
 		int fp;				//Frame Pointer
@@ -20,26 +21,44 @@ namespace SimpleVM
 			this.code = code;
 			this.ip = main;
 			data = new int[datasize];
+			globals = new int[datasize];
 			stack = new int[100];
 		}
 
 		public void cpu() {
 			while ( ip < code.Length) {
 				int opcode = code [ip];		// FETCH Operation
+
 				if (trace) {
 					disassemble (opcode);
 				}
+
 				ip++;
+
 				switch (opcode) {
 					case ByteCode.ICONST:
 						int v = code [ip++];
 						stack [++sp] = v;
 						break;
+
 					case ByteCode.PRINT:
 						v = stack [sp];
 						sp--;
-						Console.WriteLine (v);
+						Console.WriteLine (">> {0}",v);
 						break;
+
+					case ByteCode.GLOAD:
+						int addr = code [ip++];
+						stack [++sp] = globals [addr--];
+						break;
+
+					case ByteCode.GSTORE:
+						v = stack [sp];
+						sp--;
+						addr = code [ip++];
+						globals [addr] = v;
+						break;
+
 					case ByteCode.HALT:
 						return;
 				}
@@ -50,14 +69,16 @@ namespace SimpleVM
 
 		private void disassemble (int opcode) {
 			Console.Error.Write (ip+" "+ByteCode.opcodes[opcode].name);
+
 			if (ByteCode.opcodes [opcode].n == 1) {
 				Console.Error.Write (" "+code [ip+1]);
-			} else if (ByteCode.opcodes [opcode].n == 2) {
+			} 
+
+			else if (ByteCode.opcodes [opcode].n == 2) {
 				Console.Error.Write (" {0} , {1}",code[ip+1],code[ip+2]);
 			}
 
 			List<int> list = new List<int> ();
-
 			for (int i = 0; i <= sp; i++) {
 				list.Add (stack [i]);
 			}

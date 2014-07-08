5e4558113a9c496a2905ebc7342c59624adbd43a
Rashid Khan <rashood.khan@gmail.com>
2014-07-08 16:27:37 +0400

    Added VM and Test to test out the VM machine

---
+++ SimpleVM/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SimpleVM
+{
+	public class Test
+	{
+		public static void Main(){
+			VM vm= new VM (hello, 0, 0);
+			vm.cpu ();
+		}
+
+		static int[] hello = {
+			ByteCode.HALT
+		};
+
+		public Test () {
+
+		}
+	}
+}
+
---
+++ SimpleVM/VM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace SimpleVM
+{
+	public class VM
+	{
+		int[] data;			//Data Memory
+		int[] code;			//Code Memory
+		int[] stack;		//Stack
+
+		int ip;				//Instruction Pointer
+		int fp;				//Frame Pointer
+		int sp = -1;				//Stack Pointer
+
+		public VM (int[] code,int main,int datasize) {
+			this.code = code;
+			this.ip = main;
+			data = new int[datasize];
+			stack = new int[100];
+		}
+
+		public void cpu() {
+		
+		}
+	}
+}
+
